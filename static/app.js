9bf52cea123b1c6cd577c4a8010118acd8b6a343
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-17 16:14:42 +0330

    feat(statics): add open modal function

---
+++ static/app.js
@@ -0,0 +1,7 @@
+const new_data_btn = document.getElementById("new-data-btn");
+const modal = document.getElementById("modal-overlay");
+const close_btn = document.getElementById("close-btn");
+
+function open_modal(){
+    modal.classList.add("show");
+}
