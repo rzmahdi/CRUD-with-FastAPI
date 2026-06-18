90993c1a8c0f2d009534b9aae83a413de0f46e1a
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-18 12:31:07 +0330

    feat(router): add contacts post route

--- routers.py
+++ routers.py
@@ -1,3 +1,13 @@
 from fastapi.routing import APIRouter
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from database.database import get_db
+from database.models import Contact
+from database.schema import ContactResponsechema, CreateContactSchema
 
 router = APIRouter()
+
+
+@router.post("/contacts", response_model=ContactResponsechema)
+def create_contact(request: CreateContactSchema, db: Session=Depends(get_db)):
+    pass
