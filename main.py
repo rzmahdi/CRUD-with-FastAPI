c6004653e002f35ba380dd1eb3d39cb52b5eabaa
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-18 12:52:22 +0330

    feat(main): include routers

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from database.database import Base, engine
 from contextlib import asynccontextmanager
-
+import routers
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
@@ -11,3 +11,4 @@ async def life_span(app: FastAPI):
     print("App Stop!")
 
 app = FastAPI(lifespan=life_span)
+app.include_router(routers.router)
