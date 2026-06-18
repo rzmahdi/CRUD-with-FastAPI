1ec11a9069b34c36897904e342b21a41f462d4a3
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-18 12:17:21 +0330

    feat(models): add Contact model

---
+++ database/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from database.database import Base
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(50), nullable=False)
+    category = Column(String(30), nullable=False)
+    phone = Column(String(12), nullable=False)
