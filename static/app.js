57dddf56bb53956294c7cad9b43563ab799faa57
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-18 20:16:52 +0330

    feat(static-app): implement showing notifs

--- static/app.js
+++ static/app.js
@@ -4,6 +4,22 @@ const category_error_span = document.getElementById("category-error");
 const phone_error_span = document.getElementById("phone-error");
 const phone_validation_error_span = document.getElementById("phone-error-validation");
 
+const notif_modal = document.getElementById("modal-overlay-error");
+const close_notif_btn = document.getElementById("modal-error-close-btn");
+const error_modal_span = document.getElementById("error-modal-text");
+
+
+function close_modal(){
+    notif_modal.classList.remove("show");
+}
+close_notif_btn.addEventListener("click", close_modal);
+
+notif_modal.addEventListener("click", function(event){
+    if(event.target === notif_modal){
+        close_modal();
+    }
+})
+
 
 function phone_validation(phone_number){
     var regex = new RegExp("^(\\+98|0)?9\\d{9}$");
@@ -61,6 +77,15 @@ add_contact_btn.addEventListener("click", async () => {
                 phone
             })
         });
-    }
-    
+
+        if (response){
+            if(response["status"] === 409){
+                error_modal_span.innerHTML = "❌اطلاعات از قبل وجود دارد!";
+                notif_modal.classList.add("show");
+            }else{
+                error_modal_span.innerHTML = "✅اطاعات با موفقیت ثبت شد!";
+                notif_modal.classList.add("show");
+            }
+        }
+    }    
 })
