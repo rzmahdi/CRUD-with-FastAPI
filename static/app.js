df73d39d492aa69e0b1155985100c10dcb402eb3
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-18 14:36:03 +0330

    feat(static-app): raise error if inputs are empty

--- static/app.js
+++ static/app.js
@@ -1,19 +1,50 @@
 const add_contact_btn = document.getElementById("add-new-data-btn");
+const business_error_span = document.getElementById("business-name-error");
+const category_error_span = document.getElementById("category-error");
+const phone_error_span = document.getElementById("phone-error");
 
 add_contact_btn.addEventListener("click", async () => {
     const name = document.getElementById("add-business-name").value;
     const category = document.getElementById("add-category").value;
     const phone = document.getElementById("add-phone").value;
+    let have_error = false
 
-    const response = await fetch("/contacts", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name,
-            category,
-            phone
-        })
-    });
+    if(!name){
+        business_error_span.classList.add("show");
+        have_error = true;
+    }else{
+        business_error_span.classList.remove("show");
+    }
+
+    if(!category){
+        category_error_span.classList.add("show");
+        have_error = true;
+    }else{
+        category_error_span.classList.remove("show");
+    }
+
+    if(!phone){
+        phone_error_span.classList.add("show");
+        have_error = true;
+    }else{
+        phone_error_span.classList.remove("show");
+    }
+    
+    if(have_error){
+        return
+    }else{
+
+        const response = await fetch("/contacts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                category,
+                phone
+            })
+        });
+    }
+    
 })
