94ecd8eb342b17b1a6dadf74570d09399c9aa378
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-18 12:35:16 +0330

    feat(models): make Contact name unique

--- database/models.py
+++ database/models.py
@@ -6,6 +6,6 @@ class Contact(Base):
     __tablename__ = "contacts"
 
     id = Column(Integer, autoincrement=True, primary_key=True)
-    name = Column(String(50), nullable=False)
+    name = Column(String(50), nullable=False, unique=True)
     category = Column(String(30), nullable=False)
     phone = Column(String(12), nullable=False)
