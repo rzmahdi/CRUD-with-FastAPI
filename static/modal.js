8d93b512b2386ae8a7ab851dbf1b5ce81cb1848b
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-20 16:46:15 +0330

    refactor(modal): make add_data_modal display sepratly

--- static/modal.js
+++ static/modal.js
@@ -1,20 +1,31 @@
 const new_data_btn = document.getElementById("new-data-btn");
 const modal = document.getElementById("modal-overlay");
-const close_btn = document.getElementById("close-btn");
+const add_new_data_close_btn = document.getElementById("close-btn");
+const add_new_data_box = document.getElementById("modal-add-new-data-box");
 
 function open_modal(){
     modal.classList.add("show");
+}
+function open_add_data_modal(){
+    open_modal();
+    add_new_data_box.classList.add("show");
 }
 
 function close_modal(){
     modal.classList.remove("show");
 }
+function close_add_data_modal(){
+    add_new_data_box.classList.remove("show");
+    close_modal();
+}
 
-new_data_btn.addEventListener("click", open_modal);
-close_btn.addEventListener("click", close_modal);
+
+new_data_btn.addEventListener("click", open_add_data_modal);
+add_new_data_close_btn.addEventListener("click", close_add_data_modal);
 
 modal.addEventListener("click", function(event){
     if(event.target === modal){
         close_modal();
+        close_add_box();
     }
 })
